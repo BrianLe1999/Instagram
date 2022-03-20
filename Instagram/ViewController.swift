d0e91ad53fb3b0a26b5349d20545f2772ac848df
Minh Nguyen Le <minhnguyenle59@berkeley.edu>
2022-03-19 23:03:14 -0700

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 01659826174 01659826174 on 3/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
